7cdb3e20d4e740d48957f49bdb28878a10e03621
RaphaelKhalid <raphaelbahadurkhan@gmail.com>
2026-06-24 16:39:10 -0700

    Replace project card images with per-card CSS blob animations

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -29,11 +29,7 @@ const ProjectCard = ({
         className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
+          <div className={`project-card-anim anim-proj-${index} w-full h-full`} />
 
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
